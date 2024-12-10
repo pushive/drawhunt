1fe4853d195a09d31b8895f5ca4e8bff1ae7e8e5
Honsei901 <homma_wic@outlook.jp>
2024-12-10 22:00:15 +0900

    Set up server module

---
+++ src/main/kotlin/com/drawhunt/app/DrawhuntApplication.kt
@@ -0,0 +1,11 @@
+package com.drawhunt.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DrawhuntApplication
+
+fun main(args: Array<String>) {
+	runApplication<DrawhuntApplication>(*args)
+}
---
+++ src/test/kotlin/com/drawhunt/app/DrawhuntApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.drawhunt.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DrawhuntApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
