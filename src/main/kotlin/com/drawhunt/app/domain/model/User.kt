ea1511428b6a2fcde882f944475e78fba46553bb
Seiya Homma <homma_wic@outlook.jp>
2024-12-16 23:03:30 +0900

    Update User model.

--- src/main/kotlin/com/drawhunt/app/domain/model/User.kt
+++ src/main/kotlin/com/drawhunt/app/domain/model/User.kt
@@ -1,6 +1,9 @@
 package com.drawhunt.app.domain.model
 
 import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "users")
@@ -10,8 +13,22 @@ data class User(
     val id: Long = 0,
 
     @Column(name = "username", nullable = false, unique = true)
-    val username: String,
+    var username: String,
 
     @Column(name = "password", nullable = false)
-    val password: String,
+    var password: String,
+
+    @Column(name = "email_address", nullable = false, unique = true)
+    var emailAddress: String,
+
+    @Column(name = "is_confirmed", nullable = false)
+    var isConfirmed: Boolean = false,
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: LocalDateTime,
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime,
 )
